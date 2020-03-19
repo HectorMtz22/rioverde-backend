46c6ab628af52e6e83baf1bfd567cd731701b97b
Hector <hec.gre81@gmail.com>
2020-03-19 02:52:15 -0600

    Esta es la buena

--- api/products/apiproducto.php
+++ api/products/apiproducto.php
@@ -1,5 +1,8 @@
 <?php
-
+    header('Access-Control-Allow-Origin: *');
+    header("Access-Control-Allow-Headers: *");
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+    
 include_once 'producto.php';
 
 class ApiProducto{
