420bb4bf6404822c8eedfb4ea84c7b290e63edf6
Hector <hec.gre81@gmail.com>
2020-05-16 05:42:56 -0500

    Creacion de las salidas del programa

--- api/outputs/salida.php
+++ api/outputs/salida.php
@@ -5,7 +5,7 @@ include_once '../db.php';
 class Salida extends DB{
     function obtenerDetallesSalida($dateStart, $dateEnd)
     {
-        $query = $this->connect()->prepare('SELECT * FROM detallesventa WHERE (codigoventa >= :dateStart AND codigoventa <= :dateEnd)');
+        $query = $this->connect()->prepare('SELECT * FROM detallesventa WHERE (codigoventa > :dateStart AND codigoventa < :dateEnd)');
         $query->execute([
             ':dateStart' => $dateStart,
             ':dateEnd' => $dateEnd
